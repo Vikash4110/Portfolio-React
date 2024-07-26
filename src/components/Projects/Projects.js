d28eda7875601b45fd9247cd25051ae0e92e0d25
Vikash4110 <139474262+Vikash4110@users.noreply.github.com>
2024-07-26 23:02:23 +0530

    project added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,8 @@ import Particle from "../Particle";
 import ptuwebsite from "../../Assets/Projects/ptuwebsite.png";
 import childmindwell from "../../Assets/Projects/childmindwell.png";
 import convertify from "../../Assets/Projects/convertify.png";
+import School from "../../Assets/school.png";
+import Sprint from "../../Assets/sprint.png";
 
 
 function Projects() {
@@ -51,40 +53,30 @@ function Projects() {
               demoLink="https://convertifyy.netlify.app"              
             />
           </Col>
-{/* 
+
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={Sprint}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Sprint Digital"
+              description="During my internship at HopinMinds, my friend Sahil Jamwal and I were tasked with developing a comprehensive and fully responsive website. This project was designed to push our skills and knowledge, allowing us to apply and integrate a wide range of technologies and concepts in web development."
+              ghLink="https://github.com/Vikash4110/sprint_react_main_project"
+              demoLink="https://sprint-react-main-project-nine.vercel.app"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={School}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Springdale Public School"
+              description="This project is a responsive website for Springdale Public School, designed and developed as part of an assignment from the company I applied for. The website showcases various sections including Academics, Admissions, Contact Us, and more. The project was completed within 6 hours of continuous hard work."
+              ghLink="https://github.com/Vikash4110/madrocketTechnologiesSchool"
+              demoLink="https://madrocket-technologies-school.vercel.app"              
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col> */}
+
         </Row>
       </Container>
     </Container>
